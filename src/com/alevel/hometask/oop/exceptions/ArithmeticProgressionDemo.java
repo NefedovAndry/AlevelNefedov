acf042461e2b59113f8fb84674056a413a8c28b5
Nefedov Andry <Nefedov_Andry@bk.ru>
2018-08-15 10:24:43 +0300

    Update ArithmeticProgressionDemo.java

--- src/com/alevel/hometask/oop/exceptions/ArithmeticProgressionDemo.java
+++ src/com/alevel/hometask/oop/exceptions/ArithmeticProgressionDemo.java
@@ -4,10 +4,6 @@ public class ArithmeticProgressionDemo {
     public static void main(String[] args) {
         try {
             ArithmeticProgression myArithmProgr = new ArithmeticProgression(2, 5);
-        /*} catch (ProgressionConfigurationException e) {
-            System.err.println(e);
-        }
-        try {*/
             int n = myArithmProgr.calculate(2);
             System.out.println(n);
         } catch (ProgressionConfigurationException e) {
